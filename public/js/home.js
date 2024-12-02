733c2715e01d58bc088362bad5251e9dfbb0ef11
Guilherme <guilhermefortunato671@gmail.com>
2024-12-02 14:23:40 -0300

    feat: added know us and contact section;

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,23 @@
 $(()=>{
 
-    
+    /////////////
+    // Contact Section
+    const ContactSection = $('section.contact');
+    const HoverButtons = ContactSection.find('div a, div i');
+
+    HoverButtons.on('mouseover', (event)=>{
+
+        ContactSection.find('.container').find('div').removeClass('contact-active-hover');
+            
+        if($(event.target).is('a')){
+            $(event.target).parent().parent().addClass('contact-active-hover');
+            return;
+
+        }
+
+        $(event.target).parent().parent().parent().addClass('contact-active-hover');
+        return;
+
+    });
 
 });
